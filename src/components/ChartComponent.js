7d3e01eda6c8b3bf101726941e048e07bc948336
Kagamins <nsoulper@gmail.com>
2025-02-10 13:12:05 +0300

    Add files via upload

---
+++ src/components/ChartComponent.js
@@ -0,0 +1,19 @@
+'use client';
+import { Bar } from "react-chartjs-2";
+import { Chart as ChartJS, BarElement, CategoryScale, LinearScale, Title, Tooltip, Legend } from "chart.js";
+
+// Register Chart.js Components
+ChartJS.register(BarElement, CategoryScale, LinearScale, Title, Tooltip, Legend);
+
+export default function ChartComponent({ chartData, horizontal = false }) {
+    const options = {
+        indexAxis: horizontal ? "y" : "x", // **Make it horizontal**
+        responsive: true,
+        plugins: {
+            legend: { position: "top" },
+            title: { display: true, text: chartData.datasets[0].label },
+        },
+    };
+
+    return <Bar data={chartData} options={options} />;
+}
